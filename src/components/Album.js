e9eb53858cefaea1dcd0f448c2adb5226d09f786
Norman Desch <normandesch3@gmaill.com>
2019-02-21 18:06:16 -0500

    play-back

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,14 +9,45 @@ class Album extends Component {
   });
 
   this.state = {
-    album: album
+    album: album,
+   currentSong: album.songs[0],
+   isPlaying: false
   };
-}
+
+  this.audioElement = document.createElement('audio');
+  this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+     this.audioElement.pause();
+     this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
+
 
   render() {
     return (
       <section className="album">
-      <section id="album-info">
+        <section id="album-info">
          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
          <div className="album-details">
          <h1 id="album-title">{this.state.album.title}</h1>
@@ -33,9 +64,13 @@ class Album extends Component {
            <tbody>
            {
              this.state.album.songs.map((song, index) =>
-               <tr>
-                {index + 1} {song.title} {song.duration}
-               </tr>
+              <tr> className="song" key={index} onClick={() => this.handleSongClick(song)}
+              onMouseEnter={() => this.setState({ isHovered: index + 1 })}
+              onMouseLeave={() => this.setState({ isHovered: false })}>
+              (<span className={this.state.isPlaying ? "ion-pause" : "ion-play"} />) :
+							this.state.isHovered === index + 1 ? (<span className="ion-play" />) :
+							(<span className="song-number">{index + 1}</span>)}
+              </tr>
              )
            }
            </tbody>
