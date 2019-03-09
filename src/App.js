fb45ba4adaae2f88eaffdffc5daa746576e0f9cd
Norman Desch <normandesch3@gmaill.com>
2019-03-09 13:44:03 -0500

    style-css

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+//import logo from '/assets/images/bloc_jams_logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <header>
-        <nav>
-          <Link to='/'>Landing</Link>
-          <Link to='/library'>Library</Link>
-        </nav>
+          <div className="sidenav">
+             <Link to="/"><img className="logo" src="/assets/images/bloc_jams_logo.png" alt="Bloc Jams Logo"/></Link>
+             <nav>
+              <Link to="/"><span className="sidenav-icon ion-home"></span>Home</Link>
+              <Link to="/library"><span className="sidenav-icon ion-ios-albums"></span>Library</Link>
+             </nav>
+          </div>
 
-         <h1>Bloc Jams</h1>
-       </header>
-       <main>
-       <Route exact path="/" component={Landing} />
-       <Route path="/library" component={Library} />
-       <Route path="/album/:slug" component={Album} />
-       </main>
+
+            <main>
+              <Route exact path="/" component={Landing} />
+              <Route path="/library" component={Library} />
+              <Route path="/album/:slug" component={Album} />
+            </main>
       </div>
+
     );
   }
 }
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,56 +1,41 @@
 import React, { Component } from 'react';
 
- class PlayerBar extends Component {
-
-   render() {
-     return (
-       <section className="player-bar">
+class PlayerBar extends Component {
+  render() {
+    return (
+      <section className="player-bar">
         <section id="buttons">
+          <button id="previous" className="mdl-button mdl-js-button mdl-button--primary" onClick={this.props.handlePrevClick}>
+            <span className="ion-md-skip-backward"></span>
+          </button>
+          <button id="play-pause" className="mdl-button mdl-js-button mdl-button--primary" onClick={this.props.handleSongClick}>
+            <span className={this.props.isPlaying ? 'ion-md-pause' : 'ion-md-play'}></span>
+          </button>
+          <button id="next" className="mdl-button mdl-js-button mdl-button--primary" onClick={this.props.handleNextClick}>
+            <span className="ion-md-skip-forward"></span>
+          </button>
+        </section>
+        <section id="time-control">
+          <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
+          <input
+          type="range"
+          className="seek-bar mdl-slider mdl-js-slider"
+          value={(this.props.currentTime / this.props.duration) || 0.01}
+          max="1"
+          min="0"
+          step="0.01"
+          onChange={this.props.handleTimeChange}
+          />
+          <div className="total-time">{this.props.formatTime(this.props.duration)}</div>
+        </section>
+        <section id="volume-control">
+          <div className="icon ion-md-volume-low"></div>
+          <input type="range" className="seek-bar mdl-slider mdl-js-slider" max="1" min="0" step="0.01" value={this.props.currentVolume} onChange={this.props.handleVolumeChange}/>
+          <div className="icon ion-md-volume-high"></div>
+        </section>
+      </section>
+    );
+  }
+}
 
-         <button id="previous" onClick={this.props.handlePrevClick}>
-          <span className="ion-skip-backward"></span>
-         </button>
-
-         <button id="play-pause" onClick={this.props.handleSongClick} >
-           <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
-        </button>
-
-         <button id="next" onClick={this.props.handleNextClick}>
-           <span className="ion-skip-forward"></span>
-        </button>
-       </section>
-
-     <section id="time-control">
-       <div className="current-time">{this.props.currentTime}</div>
-           <input
-             type="range"
-             className="seek-bar"
-             value={(this.props.currentTime / this.props.duration) || 0}
-             max="1"
-             min="0"
-             step="0.01"
-             onChange={this.props.handleTimeChange}
-           />
-        <div className="total-time">{this.props.formatTime(this.props.currentTime)} / {this.props.formatTime(this.props.duration)}</div>
-     </section>
-
-     <section id="volume-control">
-       <div className="current-volume">{this.props.currentVolume}</div>
-       <input
-        type="range"
-        className="volume-bar"
-        value={this.props.currentVolume}
-        max="1"
-        min="0"
-        step="0.01"
-        onChange={this.props.handleVolumeChange}
-        />
-
-        <div className="total-volume">{this.props.length}</div>
-       </section>
-     </section>
-     );
-   }
- }
-
- export default PlayerBar;
+export default PlayerBar;
